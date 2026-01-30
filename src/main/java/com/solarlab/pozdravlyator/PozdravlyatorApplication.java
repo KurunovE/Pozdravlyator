26120182c2042293ee48bbf45b35ac4a51cfb081
Egor Kurunov <eegor2257@gmail.com>
2026-01-31 00:15:32 +0300

    Initial commit

---
+++ src/main/java/com/solarlab/pozdravlyator/PozdravlyatorApplication.java
@@ -0,0 +1,13 @@
+package com.solarlab.pozdravlyator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PozdravlyatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PozdravlyatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/solarlab/pozdravlyator/PozdravlyatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solarlab.pozdravlyator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PozdravlyatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
